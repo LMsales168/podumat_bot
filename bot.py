851e863ffd7a4f8b914ff26365851e7c0360837e
LMsales168 <lmsales168@gmail.com>
2025-03-05 20:36:58 +0000

    Добавил код бота

--- bot.py
+++ bot.py
@@ -22,3 +22,17 @@ def send_random_text(message):
     bot.reply_to(message, random.choice(texts))
 
 bot.polling()
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
